e3e4757b1d0a5627bba62f1bd01feb21c51592cf
Allmighty95 <santiagogiraldo2018@outlook.es>
2021-10-09 14:35:13 -0500

    se agrega el proyecto

---
+++ Servidor Web BACKEND/idx.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs");
+const home = fs.readFileSync("./index.html");
+const about = fs.readFileSync("./about.html");
+http
+  .createServer((req, res) => {
+    const { url } = req;
+    if (url === "/") {
+      res.writeHead(200, {
+        "content-Type": "text/html",
+      });
+      res.write(home);
+    } else if (url === "/about") {
+      res.writeHead(200, {
+        "content-Type": "text/html",
+      });
+      res.write(about);
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+      });
+      res.write("Page not found!");
+    }
+
+    res.end();
+  })
+  .listen(8080);
--- Servidor Web BACKEND/index.js
+++ Servidor Web BACKEND/index.js
@@ -1,8 +1,17 @@
-const http = require('http');
+const express = require("express");
+const server = express();
+const fs = require("fs");
+const home = fs.readFileSync("./index.html");
+const about = fs.readFileSync("./about.html");
+server.get("/", getHomePage);
+server.get("/about", (req, res) => {
+  res.write(about);
+});
 
-http
-    .createServer((req, res) => {
-        res.write("te amo luis");
-        res.end();
-    })
-    .listen(8080)
+server.listen(8080, () => {
+  console.log("Server is running on port" + 8080);
+});
+
+function getHomePage(req, res) {
+  return res.write(home);
+}
